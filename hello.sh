f960f0588421caa80e016af375c69f281651b55c
steven <407539793@qq.com>
2015-04-15 16:37:09 +0800

    dev

--- hello.sh
+++ hello.sh
@@ -3,3 +3,12 @@ echo "Hello World!"
 echo "What is your name?"
 read PERSON
 echo "Hello, $PERSON"
+echo "The second section 22222222"
+your_name="mozhiyan"
+echo $your_name
+echo ${your_name}
+
+for skill in Ada Coffe Action Java
+do
+    echo "I am good at ${skill}Script"
+done
